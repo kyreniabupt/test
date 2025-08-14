cdccc2d20731c8f010fb8eaae29f2f4c914b9713
kyreniabupt <Kyreniaws@163.com>
2025-08-14 20:00:48 +0800

    initial commit

---
+++ dataset.py
@@ -0,0 +1,43 @@
+from torch.utils.data import Dataset
+import torch
+import json
+
+# Custom Dataset class
+class SentencePairDataset(Dataset):
+    def __init__(self, file_path, tokenizer, max_length):
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+        self.data = []
+
+        with open(file_path, 'r', encoding='utf-8') as f:
+            for line in f:
+                item = json.loads(line.strip())
+                self.data.append(item)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        item = self.data[idx]
+
+        instruction_text = item["instruction"]
+        label_text = item["output"]
+        label = 1 if label_text == 'True' else 0
+
+        encoding = self.tokenizer(
+            instruction_text,
+            max_length=self.max_length,
+            padding=False,
+            truncation=True,
+            return_tensors='pt'
+        )
+
+        return {
+            'input_ids': encoding['input_ids'].flatten(),
+            'attention_mask': encoding['attention_mask'].flatten(),
+            'label': torch.tensor(label, dtype=torch.long)
+        }
+
+class SentencePairPredictDataset(SentencePairDataset):
+    def __getitem__(self, idx):
+        return self.data[idx]
